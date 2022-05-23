07a3493860928ac1118db7cc5b1cc63237a763bd
alexiskoa <103915224+alexiskoa@users.noreply.github.com>
2022-05-23 14:33:16 +0800

    Add files via upload

---
+++ assignment-0.py.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+'''Assignment 0
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+    '''Item 1.
+    Three Number Average. 3 points.
+    Returns the average of three numbers.
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this line
+    avg = (x + y + z)/3
+    return avg
+
+sample_numbers = three_number_average(10, 20, 30)
+print(sample_numbers)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
